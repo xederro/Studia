14c5a0f86ebd4a64366fbef8644d8ae9267d1b11
xederro <50398562+xederro@users.noreply.github.com>
2022-10-06 14:54:03 +0200

    vault backup: 2022-10-06 14:54:02
    Affected files:
    Projekty/Podstawy programowania/Labolatoria/Labolatorium 0/Zadanie1.cpp
    Projekty/Podstawy programowania/Labolatoria/Labolatorium 0/Zadanie2.1.cpp
    Projekty/Podstawy programowania/Labolatoria/Labolatorium 0/Zadanie2.2.cpp
    Projekty/Podstawy programowania/Labolatoria/Labolatorium 0/Zadanie2.cpp
    Projekty/Podstawy programowania/Labolatoria/Labolatorium 0/Zadanie3.1.cpp
    Projekty/Podstawy programowania/Labolatoria/Labolatorium 0/Zadanie3.2.cpp
    Projekty/Podstawy programowania/Labolatoria/Labolatorium 0/Zadanie3.cpp

--- Projekty/Podstawy programowania/Labolatoria/Labolatorium 0/Zadanie1.cpp
+++ Projekty/Podstawy programowania/Labolatoria/Labolatorium 0/Zadanie1.cpp
@@ -1,8 +1,8 @@
 /*
 Autor:  Dawid Jabłoński
 Grupa:  WT/NP 11.15
-Temat:  Zadanie 1 z Laboratorium 0
-Data:   4 października 2022r.
+Tytuł:  Zadanie 1, Laboratorium 0
+Data:   6 października 2022r.
 */
 
 #include <iostream>
@@ -13,19 +13,33 @@ using namespace std;
 int main()
 {
     // OBOWIĄZKOWY wydruk danych autora
-    printf("Autor: Dawid Jabłoński (WT/NP 11:15) \n\n\n");
+    printf("Autor: Dawid Jabłoński (WT/NP 11:15)");
 
-    // Podpunkt A
+    /*
+    Podpunkt A
+    iostream
+    */
+    printf("\n\n\nWariant 1 iostream\n\n");
+
+    // Podpunkt A iostream
     cout << "Dawid Jabłoński" << endl;
     cout << "Edwarda Wittiga 6, \n51-628 Wrocław" << endl;
     cout << "+48 643 938 263" << endl;
     cout << "student@example.com" << endl;
 
-    cout << "\n\n\n\n";
+    system("PAUSE");
+    /*
+    Podpunkt A
+    iostream
+    */
+    printf("\n\n\nWariant 2 stdio.h\n\n");
 
-    // Podpunkt B
+    // Podpunkt B stdio.h
     printf("Adam Kowalski\n");
     printf("Janiszewskiego 11/17, \n50-372 Wrocław\n");
     printf("+48 123 456 789\n");
     printf("admin@example.com\n");
+
+    system("PAUSE");
+    return 0;
 }
