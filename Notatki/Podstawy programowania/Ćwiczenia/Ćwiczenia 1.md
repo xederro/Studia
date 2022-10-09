c0bb257f029720b038e72176ae644e90c9dc0c80
xederro <50398562+xederro@users.noreply.github.com>
2022-10-09 20:54:27 +0200

    vault backup: 2022-10-09 20:54:27
    Affected files:
    .obsidian/workspace
    Notatki/Podstawy programowania/Ćwiczenia/Untitled Diagram.svg
    Notatki/Podstawy programowania/Ćwiczenia/cw1zad4.svg
    Notatki/Podstawy programowania/Ćwiczenia/Ćwiczenia 1.md
    Projekty/Podstawy programowania/Labolatoria/template.cpp

--- Notatki/Podstawy programowania/Ćwiczenia/Ćwiczenia 1.md
+++ Notatki/Podstawy programowania/Ćwiczenia/Ćwiczenia 1.md
@@ -10,4 +10,25 @@
 3. 
    ![[cw1zad3.svg]]
 4. 
-   ![[Notatki/Podstawy programowania/Ćwiczenia/Untitled Diagram.svg]]
+   ![[cw1zad4.svg]]
+5. 
+ | Wartość zmiennej                   | Typ                | Ile zajmuje bajtów | Kod formujący dla funkcji printf |
+ | ---------------------------------- | ------------------ | ------------------ | -------------------------------- |
+ | A                                  | char               | 1                  | %c                               |
+ | -32768 … 32767                     | short int          | 2                  | %sd                              |
+ | 0 ... 65535                        | unsigned short int | 2                  | %su                              |
+ | -2147483648 … 2147483647           | int                | 4                  | %d                               |
+ | $-3,4*10^{-38} \cdots 3,4*10^{38}$ | float              | 4                  | $f                               |
+ | $10^{-308} \cdots 10^{308}$        | double             | 8                  | %ld                              |
+ |                                    |                    |                    |                                  |
+6.  a.
+   - %o
+   - %x
+   - $6d
+     b.
+   - %.3f
+   - %.0f
+   - %8.2f
+   - %E
+
+7. 
--- Projekty/Podstawy programowania/Labolatoria/template.cpp
+++ Projekty/Podstawy programowania/Labolatoria/template.cpp
@@ -16,46 +16,9 @@ int main()
     // OBOWIĄZKOWY wydruk danych autora
     printf("Autor: Dawid Jabłoński (WT/NP 11:15) \n\n");
 
-    // Deklaracja zmiennych lokalnych typu int i float
-    int a;
-    float b;
+    double test = 555555.555555556;
 
-    // Przypisanie zmiennycm warto��i pocz�tkowych
-    a = 2;
-    b = 3.5;
-
-    // PRZYK�AD PIERWSZY
-    // Drukowanie komunikat�w z wykorzystaniem biblioteki iostream   (Tylko C++)
-    cout << "To jest przyklad uzycia biblioteki iostream. " << endl;
-    cout << "Zmienna a ma wartosc " << a << endl;
-    cout << "Zmienna b ma wartosc " << b << endl;
-    cout << "suma a+b wynosi " << a + b << endl
-         << endl;
-
-    // Wczytywanie danych z wykorzystaniem biblioteki iostream    (Tylko C++)
-    cout << "Podaj nowa wartosc a: ";
-    cin >> a;
-    cout << "Podaj nowa wartosc b: ";
-    cin >> b;
-    cout << "Suma a+b wynosi " << a + b << endl;
-
-    system("PAUSE");
-
-    // PRZYK�AD DRUGI
-    // Drukowanie komunikat�w z wykorzystaniem biblioteki stdio.h    (C , C++)
-    printf("\n");
-    printf("To jest przyklad uzycia biblioteki stdio.h. \n");
-    printf("Zmienna a ma wartosc %d \n", a);
-    printf("Zmienna b ma wartosc %f \n", b);
-    printf("Suma a+b wynosi %f \n\n", a + b);
-
-    printf("Podaj nowa wartosc a: ");
-    scanf("%d", &a);
-    printf("Podaj nowa wartosc b: ");
-    scanf("%f", &b);
-    printf("Suma a+b wynosi %f \n\n", a + b);
-
-    system("PAUSE");
+    printf("%E", test);
 
     return 0;
 }
