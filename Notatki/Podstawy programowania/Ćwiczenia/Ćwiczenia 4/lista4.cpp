b0167cf09700e45884daabb9b8ae6bde5508a8a6
xederro <50398562+xederro@users.noreply.github.com>
2022-11-20 14:06:23 +0100

    vault backup: 2022-11-20 14:06:23
    Affected files:
    .obsidian/workspace.json
    Notatki/Podstawy programowania/Ćwiczenia/Ćwiczenia 4/lista4.cpp

--- Notatki/Podstawy programowania/Ćwiczenia/Ćwiczenia 4/lista4.cpp
+++ Notatki/Podstawy programowania/Ćwiczenia/Ćwiczenia 4/lista4.cpp
@@ -3,158 +3,162 @@
 
 int main()
 {
-    //##########################################################################
-    //       PRZYK£AD 1 - prosta zmienna
-    //##########################################################################
-    
-    printf("Przyklad 1: prosta zmienna\n");
-    short x = 6;    //  deklaracja zmiennej x
-    short *wsk;     //  deklaracja zmiennej wskaŸnikowej wsk
+        //##########################################################################
+        //       PRZYKï¿½AD 1 - prosta zmienna
+        //##########################################################################
 
-    wsk = &x;       // adres zmiennj x
-    
-    printf("      x = %-8d      sizeof(x) = %d \n", x, sizeof(x));
-    printf("    wsk = %p    sizeof(wsk) = %d    sizeof(*wsk) = %d \n", 
-           wsk, sizeof(wsk), sizeof(*wsk));
-    printf("      x = %d \n",  x );       // bezpoœrednie odwo³anie do zmiennej 
-    printf("   *wsk = %d \n",  *wsk );    // odwo³anie za pomoc¹ wska¿nika
-    printf(" wsk[0] = %d \n",  wsk[0] );  // odwo³anie za indeksu
-    
-    
-    
-    //##########################################################################
-    //       PRZYK£AD 2 - tablica jednowymiarowa
-    //##########################################################################
-    
-    printf("\n\n");
-    printf("Przyklad 2: trzyelementowa tablica liczb\n");
-    short tab1[3] = {1, 3, 9};  // 3-elementowa tablica typu short
-    short *wsk1;                // wskaŸnik na typ short
-    short (*wsk2)[3];           // wskaŸnik na 3-elementow¹ tablicê 
-                                // typu short 
-                          
-    wsk1 = tab1;                //  adres pierwszego elementu tablicy
-    wsk2 = &tab1;               // adres 3-elementowej tablicy
+        printf("Przyklad 1: prosta zmienna\n");
+        short x = 6;    //  deklaracja zmiennej x
+        short* wsk;     //  deklaracja zmiennej wskaï¿½nikowej wsk
 
-   
-    // wydruk wartoœci wskaŸników oraz rozmiaru danych, na które wskazuj¹ 
-    printf("    tab1 = %p    sizeof(tab1) = %d    sizeof(*tab1) = %d \n",   
-            tab1, sizeof(tab1), sizeof(*tab1));
-    printf("    wsk1 = %p    sizeof(wsk1) = %d    sizeof(*wsk1) = %d \n",   
-            wsk1, sizeof(wsk1), sizeof(*wsk1));
-    printf("    wsk2 = %p    sizeof(wsk2) = %d    sizeof(*wsk2) = %d \n\n", 
-            wsk2, sizeof(wsk2), sizeof(*wsk2));
-    
-    // wydruk wartoœci elementu tablicy oznaczonego kolorem szarym
-    printf("     tab1[2] = %d\n",  tab1[2]      );  // notacja indeksowa
-    printf("   *(tab1+2) = %d\n",  *(tab1+2)    );  // notacja wskaŸnikowa
-    printf("     wsk1[2] = %d\n",  wsk1[2]      );  // notacja indeksowa
-    printf("   *(wsk1+2) = %d\n",  *(wsk1+2)    );  // notacja wskaŸnikowa
-    printf("  wsk2[0][2] = %d\n",  wsk2[0][2]   );  // notacja indeksowa
-    printf(" *(*wsk2 +2) = %d\n",  *(*wsk2+2)   );  // notacja wskaŸnikowa
+        wsk = &x;       // adres zmiennj x
 
-    
-    //##########################################################################
-    //       PRZYK£AD 3 - dwuwymiarowa tablica liczb
-    //##########################################################################
-    
-    printf("\n\n");
-    printf("Przyklad 3: dwuwymiarowa tablica liczb\n");
-    
-    int tab2[4][3] = {{1,7,5},{1,4,0},{2,5,9},{8,9,0}};
+        printf("      x = %-8d      sizeof(x) = %d \n", x, sizeof(x));
+        printf("    wsk = %p    sizeof(wsk) = %d    sizeof(*wsk) = %d \n",
+                wsk, sizeof(wsk), sizeof(*wsk));
+        printf("      x = %d \n", x);       // bezpoï¿½rednie odwoï¿½anie do zmiennej 
+        printf("   *wsk = %d \n", *wsk);    // odwoï¿½anie za pomocï¿½ wskaï¿½nika
+        printf(" wsk[0] = %d \n", wsk[0]);  // odwoï¿½anie za indeksu
 
-    // Tu proszê dopisaæ deklaracjê wskaŸników wsk3, wsk4, wsk5
-    
-    
-    
-    
-   
-    // Tu proszê nadaæ wartoœci wskaŸnikom wsk3, wsk4, wsk5
-    
-    
-    
-    
-    
-    
-    // Tu proszê dopisaæ wydruk wartoœci wskaŸników tab2, wsk3, wsk4, wsk5
-    // oraz rozmiaru danych,  na które wskazuj¹. 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-            
-    // Tu proszê dopisaæ wydruk wartoœci elementu tablicy oznaczonego
-    // kolorem szarym. odwo³ania do tego elementu proszê robiæ za pomoc¹
-    // identyfikatora tab2 oraz wskaŸników wsk3, wsk4, wsk5. W ka¿dym
-    // przypadku proszê zrobiæ dwa warianty (z u¿yciem notacji indeksowej 
-    // oraz notacji wskaŸnikowej)        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 
-    
-    //##########################################################################
-    //       PRZYK£AD 4 - dwywymiarowa tablica o zmiennaj liczbie kolumn
-    //##########################################################################
-    
-    printf("\n\n");
-    printf("Przyklad 4: dwuwymiarowa tablica liczb,\n");
-    printf("ktora zawiera trzy wiersze o roznaj liczbie kolumn\n");
-    
-    
-    char tab3a[] = {1, 5, 0};
-    char tab3b[] = {6, 2, 9, 8, 1, 0};
-    char tab3c[] = {3, 4, 7, 0};
 
-    char *tab3[3] = {tab3a, tab3b, tab3c};
-    
-    // Tu proszê dopisaæ deklaracjê wskaŸników wsk6, wsk7
-    
-    
-    
+        //##########################################################################
+        //       PRZYKï¿½AD 2 - tablica jednowymiarowa
+        //##########################################################################
 
-    
-    // Tu proszê nadaæ wartoœci wskaŸnikom wsk6, wsk7
-    
-    
-    
-    
-    
-    // Tu proszê dopisaæ wydruk wartoœci wskaŸników tab3, wsk6, wsk7
-    // oraz rozmiaru danych,  na które wskazuj¹.
-    
-    
-    
-    
-    
-    
+        printf("\n\n");
+        printf("Przyklad 2: trzyelementowa tablica liczb\n");
+        short tab1[3] = { 1, 3, 9 };  // 3-elementowa tablica typu short
+        short* wsk1;                // wskaï¿½nik na typ short
+        short(*wsk2)[3];           // wskaï¿½nik na 3-elementowï¿½ tablicï¿½ 
+        // typu short 
 
-    // Tu proszê dopisaæ wydruk wartoœci elementu tablicy oznaczonego
-    // kolorem szarym. odwo³ania do tego elementu proszê robiæ za pomoc¹
-    // identyfikatora tab3 oraz wskaŸników wsk6, wsk7. W ka¿dym
-    // przypadku proszê zrobiæ dwa warianty (z u¿yciem notacji indeksowej 
-    // oraz notacji wskaŸnikowej)                  
-   
-    
-    
-    
-    
-    
-    
-    
-    
-       
-    return 0;       
+        wsk1 = tab1;                //  adres pierwszego elementu tablicy
+        wsk2 = &tab1;               // adres 3-elementowej tablicy
+
+
+        // wydruk wartoï¿½ci wskaï¿½nikï¿½w oraz rozmiaru danych, na ktï¿½re wskazujï¿½ 
+        printf("    tab1 = %p    sizeof(tab1) = %d    sizeof(*tab1) = %d \n",
+                tab1, sizeof(tab1), sizeof(*tab1));
+        printf("    wsk1 = %p    sizeof(wsk1) = %d    sizeof(*wsk1) = %d \n",
+                wsk1, sizeof(wsk1), sizeof(*wsk1));
+        printf("    wsk2 = %p    sizeof(wsk2) = %d    sizeof(*wsk2) = %d \n\n",
+                wsk2, sizeof(wsk2), sizeof(*wsk2));
+
+        // wydruk wartoï¿½ci elementu tablicy oznaczonego kolorem szarym
+        printf("     tab1[2] = %d\n", tab1[2]);  // notacja indeksowa
+        printf("   *(tab1+2) = %d\n", *(tab1 + 2));  // notacja wskaï¿½nikowa
+        printf("     wsk1[2] = %d\n", wsk1[2]);  // notacja indeksowa
+        printf("   *(wsk1+2) = %d\n", *(wsk1 + 2));  // notacja wskaï¿½nikowa
+        printf("  wsk2[0][2] = %d\n", wsk2[0][2]);  // notacja indeksowa
+        printf(" *(*wsk2 +2) = %d\n", *(*wsk2 + 2));  // notacja wskaï¿½nikowa
+
+
+        //##########################################################################
+        //       PRZYKï¿½AD 3 - dwuwymiarowa tablica liczb
+        //##########################################################################
+
+        printf("\n\n");
+        printf("Przyklad 3: dwuwymiarowa tablica liczb\n");
+
+        int tab2[4][3] = { {1,7,5},{1,4,0},{2,5,9},{8,9,0} };
+
+        // Tu proszï¿½ dopisaï¿½ deklaracjï¿½ wskaï¿½nikï¿½w wsk3, wsk4, wsk5
+
+        int* wsk3 = tab2;
+        int(*wsk4)[3] = &(tab2[0]);
+        int(*wsk5)[4][3] = &tab2;
+
+
+
+
+        // Tu proszï¿½ nadaï¿½ wartoï¿½ci wskaï¿½nikom wsk3, wsk4, wsk5
+
+        wsk3 = 1;
+        wsk4 = 2;
+        wsk5 = 3;
+
+
+        // Tu proszï¿½ dopisaï¿½ wydruk wartoï¿½ci wskaï¿½nikï¿½w tab2, wsk3, wsk4, wsk5
+        // oraz rozmiaru danych,  na ktï¿½re wskazujï¿½. 
+
+
+        printf("    tab2 = %p    sizeof(tab2) = %d    sizeof(*tab2) = %d \n",
+                tab2, sizeof(tab2), sizeof(*tab2));
+        printf("    wsk3 = %p    sizeof(wsk3) = %d    sizeof(*wsk3) = %d \n",
+                wsk3, sizeof(wsk3), sizeof(*wsk3));
+        printf("    wsk4 = %p    sizeof(wsk4) = %d    sizeof(*wsk4) = %d \n",
+                wsk4, sizeof(wsk4), sizeof(*wsk4));
+        printf("    wsk5 = %p    sizeof(wsk5) = %d    sizeof(*wsk5) = %d \n\n",
+                wsk5, sizeof(wsk5), sizeof(*wsk5));
+
+
+        // Tu proszï¿½ dopisaï¿½ wydruk wartoï¿½ci elementu tablicy oznaczonego
+        // kolorem szarym. odwoï¿½ania do tego elementu proszï¿½ robiï¿½ za pomocï¿½
+        // identyfikatora tab2 oraz wskaï¿½nikï¿½w wsk3, wsk4, wsk5. W kaï¿½dym
+        // przypadku proszï¿½ zrobiï¿½ dwa warianty (z uï¿½yciem notacji indeksowej 
+        // oraz notacji wskaï¿½nikowej)        
+
+        printf("     tab2[3][0] = %d\n", tab2[3][0]);
+        printf("     *(wsk3+(3*3)) = %d\n", *(wsk3 + (3 * 3)));
+        printf("     wsk3[3][0] = %d\n", wsk3[3][0]);
+        printf("     *(wsk4+(3*3)) = %d\n", *(wsk4 + (3 * 3)));
+        printf("     wsk4[3][0] = %d\n", wsk4[3][0]);
+        printf("     *(wsk5 + (3 * 3)) = %d\n", *(wsk5 + (3 * 3)));
+        printf("     wsk5[3][0] = %d\n", wsk5[3][0]);
+
+
+
+        //##########################################################################
+        //       PRZYKï¿½AD 4 - dwywymiarowa tablica o zmiennaj liczbie kolumn
+        //##########################################################################
+
+        printf("\n\n");
+        printf("Przyklad 4: dwuwymiarowa tablica liczb,\n");
+        printf("ktora zawiera trzy wiersze o roznaj liczbie kolumn\n");
+
+
+        char tab3a[] = { 1, 5, 0 };
+        char tab3b[] = { 6, 2, 9, 8, 1, 0 };
+        char tab3c[] = { 3, 4, 7, 0 };
+
+        char* tab3[3] = { tab3a, tab3b, tab3c };
+
+        // Tu proszï¿½ dopisaï¿½ deklaracjï¿½ wskaï¿½nikï¿½w wsk6, wsk7
+
+
+
+
+
+        // Tu proszï¿½ nadaï¿½ wartoï¿½ci wskaï¿½nikom wsk6, wsk7
+
+
+
+
+
+        // Tu proszï¿½ dopisaï¿½ wydruk wartoï¿½ci wskaï¿½nikï¿½w tab3, wsk6, wsk7
+        // oraz rozmiaru danych,  na ktï¿½re wskazujï¿½.
+
+
+
+
+
+
+
+        // Tu proszï¿½ dopisaï¿½ wydruk wartoï¿½ci elementu tablicy oznaczonego
+        // kolorem szarym. odwoï¿½ania do tego elementu proszï¿½ robiï¿½ za pomocï¿½
+        // identyfikatora tab3 oraz wskaï¿½nikï¿½w wsk6, wsk7. W kaï¿½dym
+        // przypadku proszï¿½ zrobiï¿½ dwa warianty (z uï¿½yciem notacji indeksowej 
+        // oraz notacji wskaï¿½nikowej)                  
+
+
+
+
+
+
+
+
+
+
+        return 0;
 }
