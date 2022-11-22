87739bce34a5ef9e061634817d25f8ce1d56c3a3
xederro <50398562+xederro@users.noreply.github.com>
2022-11-22 17:12:52 +0100

    vault backup: 2022-11-22 17:12:52
    Affected files:
    .obsidian/workspace.json
    Notatki/Podstawy programowania/Labolatoria/Labolatoria 4/Labolatoria4.1.cpp
    Notatki/Podstawy programowania/Labolatoria/Labolatoria 4/Labolatoria4.1.exe
    Notatki/Podstawy programowania/Labolatoria/Labolatoria 4/Labolatoria4.2.cpp
    Notatki/Podstawy programowania/Labolatoria/Labolatoria 4/Labolatoria4.2.exe
    Notatki/Podstawy programowania/Labolatoria/Labolatoria 4/Labolatorium 4.cpp
    Notatki/Podstawy programowania/Labolatoria/Labolatoria 4/Labolatorium 4.exe

---
+++ Notatki/Podstawy programowania/Labolatoria/Labolatoria 4/Labolatoria4.1.cpp
@@ -0,0 +1,92 @@
+/*
+Autor:  Dawid Jabłoński
+Grupa:  WT/N 11.15
+Tytul:  Zadania Laboratorium 4
+Data:   22 listopada 2022r.
+*/
+
+#include <stdio.h>
+
+void CzytajRownanie(float& a, float& b, float& c);
+void WypiszRozwiazanie(int N, float x, float y);
+float ObliczWyznacznik(float p1, float p2, float p3, float p4);
+int ObliczRozwiazanie(float a1, float b1, float c1, float a2, float b2, float c2, float& x, float& y);
+
+int main() {
+
+    // OBOWIAZKOWY wydruk danych autora
+    printf("Autor: Dawid Jablonski (WT/N 11:15)\n");
+
+
+    float A1, B1, C1; // pierwsze równanie,
+    float A2, B2, C2; // drugie równanie,
+    float X, Y; // rozwiązanie,
+    int N; // liczba rozwiazan.
+
+    CzytajRownanie(A1, B1, C1);
+    CzytajRownanie(A2, B2, C2);
+    N = ObliczRozwiazanie(A1, B1, C1, A2, B2, C2, X, Y);
+    WypiszRozwiazanie(N, X, Y);
+
+    return 0;
+}
+
+void CzytajRownanie(float& a, float& b, float& c) {
+    printf("Podaj A funkcji: Ax+By=C\n");
+    scanf("%f", &a);
+    printf("Podaj B funkcji: Ax+By=C\n");
+    scanf("%f", &b);
+    printf("Podaj C funkcji: Ax+By=C\n");
+    scanf("%f", &c);
+}
+
+void WypiszRozwiazanie(int N, float x, float y) {
+    switch (N)
+    {
+    case -1:
+        printf("Wystapil problem w funkcji ObliczWyznacznik\n");
+        break;
+    case 0:
+        printf("Rownanie nie posiada rozwiazan\n");
+        break;
+
+    case 1:
+        printf("Rownanie ma dokladnie 1 rozwiazanie: x=%f oraz y=%f\n", x, y);
+        break;
+
+    case 2:
+        printf("Rownanie ma nieskonczenie wiele rozwazan\n");
+        break;
+
+    default:
+        printf("Wystapil problem w funkcji WypiszRozwiazanie\n");
+        break;
+    }
+}
+
+float ObliczWyznacznik(float p1, float p2, float p3, float p4) {
+    return (p1 * p4) - (p2 * p3);
+}
+
+int ObliczRozwiazanie(float a1, float b1, float c1, float a2, float b2, float c2, float& x, float& y) {
+    float wyznacznik = ObliczWyznacznik(a1, b1, a2, b2);
+    float wyznacznikX = ObliczWyznacznik(c1, b1, c2, b2);
+    float wyznacznikY = ObliczWyznacznik(a1, c1, a2, c2);
+
+    if (wyznacznik != 0) // gdy różne 0
+    {
+        x = wyznacznikX / wyznacznik;
+        y = wyznacznikY / wyznacznik;
+        return 1;
+    }
+
+    if (wyznacznikX == 0 && wyznacznikY == 0) {
+        return 2;
+    }
+
+    if (wyznacznikX != 0 && wyznacznikY != 0) {
+        return 0;
+    }
+
+    return -1;
+}
--- Notatki/Podstawy programowania/Labolatoria/Labolatoria 4/Labolatorium 4.cpp
+++ Notatki/Podstawy programowania/Labolatoria/Labolatoria 4/Labolatorium 4.cpp
@@ -22,6 +22,28 @@ wypisać jako %p na 16 zamieni
 
 */
 
+void zadanie2(char(*tab)[100]) {
+    for (char i = 0; (*tab)[i] != 0; i++) {
+        if ((*tab)[i] < '9' && (*tab)[i] > '0') {
+            for (char k = i; (*tab)[i] != 0; k++)
+            {
+                (*tab)[i] = (*tab)[i + 1];
+            }
+        }
+    }
+}
+
+
+void zadanie3() {
+
+}
+
+
+void zadanie4() {
+
+}
+
+
 int main() {
 
     // OBOWIAZKOWY wydruk danych autora
@@ -32,7 +54,7 @@ int main() {
     unsigned short int wybor = 0;
     do
     {
-        printf("\n0- Wyjscie z programu\n1- Zadanie 1\n2- Zadanie 2\n3- Zadanie 3\n\n");
+        printf("\n0- Wyjscie z programu\n1- Zadanie 2\n2- Zadanie 3\n3- Zadanie 4\n\n");
         scanf("%hhd", &wybor);
 
         switch (wybor)
@@ -40,23 +62,30 @@ int main() {
         case 0:
             printf("Do widzenia");
             break;
-            // case 1:
-            //     /*
-            //         Zadanie 1
-            //     */
-            //     zadanie1();
-            //     break;
+        case 1:
+            /*
+                Zadanie 2
+            */
+            char tab[100];
+            printf("Wbisz zdanie:\n");
+
+            scanf("%s", &tab);
+            zadanie2(&tab);
+            for (char i = 0; tab[i] != 0; i++) {
+                printf("%c\n", tab[i]);
+            }
+            break;
             // case 2:
-            //     /*
-            //         Zadanie 2
-            //     */
-            //     zadanie2();
-            //     break;
-            // case 3:
             //     /*
             //         Zadanie 3
             //     */
             //     zadanie3();
+            //     break;
+            // case 3:
+            //     /*
+            //         Zadanie 4
+            //     */
+            //     zadanie4();
             //     break;
 
         default:
