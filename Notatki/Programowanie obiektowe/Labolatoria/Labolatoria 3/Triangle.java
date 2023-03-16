cc774928728c5ae7de0804e87e3fb7f086ee3a81
xederro <50398562+xederro@users.noreply.github.com>
2023-03-16 09:21:48 +0100

    vault backup: 2023-03-16 09:21:48
    Affected files:
    .obsidian/workspace.json
    Notatki/Matematyka dyskretna/Wykłady/Wykład 1/Drawing 2023-03-16 09.21.17.excalidraw.md
    Notatki/Programowanie obiektowe/Labolatoria/Labolatoria 3/Labolatoria 3.md
    Notatki/Programowanie obiektowe/Labolatoria/Labolatoria 3/Triangle.java

--- Notatki/Programowanie obiektowe/Labolatoria/Labolatoria 3/Triangle.java
+++ Notatki/Programowanie obiektowe/Labolatoria/Labolatoria 3/Triangle.java
@@ -1,43 +1,124 @@
 public class Triangle {
-    public static void print_triangle(int N){
-        int[][] pascal = new int[N+1][N+1];
+    public static void print_triangle(int N) {
+        Integer[][] pascal = new Integer[N+1][N+1];
+        int widest = 1;
+
 
         if (N >= 0) {
             pascal[0][0] = 1;
         }
+
         if (N >= 1) {
             pascal[1][0] = 1;
             pascal[1][1] = 1;
         }
-        if (N >= 2) {
-            for (int i = 2; i <= N; i++) {
-                for (int j = i; j >= 0; j--) {
-                    if (j == i || j == 0) {
-                        pascal[i][j] = 1;
-                    } else {
-                        pascal[i][j] = pascal[i-1][j]+pascal[i-1][j-1];
-                    }
+
+        for (int i = 2; i <= N; i++){
+            for (int j = i; j >= 0; j--){
+                if (j==0 || j==i){
+                    pascal[i][j] = 1;
+                } else {
+                    pascal[i][j]=pascal[i-1][j]+pascal[i-1][j-1];
+                }
+
+                if(pascal[i][j].toString().length() >= widest) {
+                    widest = pascal[i][j].toString().length();
                 }
             }
         }
 
         for (int i = 0; i <= N; i++) {
-            for (int j = N-i; j >= 0; j--) {
-                System.out.print("\t");
+            for (int j = N-i; j > 0; j--) {
+                for (int k = widest; k > 0; k--){
+                    System.out.print(" ");
+                }
+                System.out.print("0");
             }
 
             for (int j = 0; j <= i; j++) {
-                System.out.printf("%d\t", pascal[i][j]);
-                if (j != i){
-                    System.out.print("0\t");
+                for (int k = widest - pascal[i][j].toString().length(); k >= 0; k--){
+                    System.out.print(" ");
                 }
+                System.out.printf("%d", pascal[i][j]);
+                for (int k = widest; k > 0; k--){
+                    System.out.print(" ");
+                }
+                System.out.print("0");
+            }
+
+            for (int j = N-i; j >= 0; j--) {
+                for (int k = widest; k > 0; k--){
+                    System.out.print(" ");
+                }
+                System.out.print("0");
             }
 
             System.out.print("\n");
         }
     }
 
+    public static void print_triangle_but_better(int N) {
+        Integer[][] pascal = new Integer[N+1][N+1];
+        int widest = 1;
+
+
+        if (N >= 0) {
+            pascal[0][0] = 1;
+        }
+
+        if (N >= 1) {
+            pascal[1][0] = 1;
+            pascal[1][1] = 1;
+        }
+
+        for (int i = 2; i <= N; i++){
+            for (int j = i; j >= 0; j--){
+                if (j==0 || j==i){
+                    pascal[i][j] = 1;
+                } else {
+                    pascal[i][j]=pascal[i-1][j]+pascal[i-1][j-1];
+                }
+
+                if(pascal[i][j].toString().length() >= widest) {
+                    widest = pascal[i][j].toString().length();
+                }
+            }
+        }
+
+        for (int i = 0; i <= N; i++) {
+            for (int j = N-i; j > 0; j--) {
+                for (int k = widest; k >= 0; k--){
+                    System.out.print(" ");
+                }
+            }
+
+            for (int j = 0; j <= i; j++) {
+                for (int k = widest - pascal[i][j].toString().length(); k >= 0; k--){
+                    System.out.print(" ");
+                }
+                System.out.printf("%d", pascal[i][j]);
+                for (int k = widest; k >= 0; k--){
+                    System.out.print(" ");
+                }
+            }
+
+            for (int k = widest; k >= 0; k--){
+                System.out.print(" ");
+            }
+
+            System.out.print("\n");
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        print_triangle(10);
+        print_triangle(21);
+
+        System.out.print("\n\n\n\n\n\n");
+
+        print_triangle_but_better(21);
+
+
     }
 }
